b3d51b0da8ce404861ea7f448d95746de621b9a0
Anmol Bhagat <anmolbhagat.dev@gmail.com>
2026-08-14 22:48:14 +0530

    feat: add vt feed card structure

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -9,108 +9,140 @@ import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
 import { Link } from 'react-router-dom'
-
+import channel from '../../assets/channel.jpg'
+import options from '../../assets/options.svg'
 const Feed = () => {
     return (
-    <div className='feed'>
-        <Link to={`video/20/4521`} className="card">
-            <img src={thumbnail1} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </Link>
-        <div className="card">
-            <img src={thumbnail2} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail3} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail4} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail5} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail6} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail7} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail8} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail1} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail2} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail3} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail4} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail5} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail6} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail7} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-        <div className="card">
-            <img src={thumbnail8} alt="" />
-            <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
-            <h3>Greatstack</h3>
-            <p>15k views &bull; 2 days ago</p>
-        </div>
-    </div >
-  )
+        <div className='feed'>
+            <Link to={`video/20/4521`} className="card">
+                <div className="video">
+                    <img src={thumbnail1} alt="" />
+                </div>
+                <div className="video-info">
+                    <div className="video-info-left">
+                        <img src={channel} alt="" />
+                    </div>
+                    <div className="video-info-right">
+                        <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
+                        <h3>Greatstack</h3>
+                        <p>15k views &bull; 2 days ago</p>
+                    </div>
+                    <button><img className='options' src={options} alt="" /></button>
+                </div>
+            </Link>
+            <div className="card">
+                <div className="video">
+                    <img src={thumbnail2} alt="" />
+                </div>
+                <div className="video-info">
+                    <div className="video-info-left">
+                        <img src={channel} alt="" />
+                    </div>
+                    <div className="video-info-right">
+                        <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
+                        <h3>Greatstack</h3>
+                        <p>15k views &bull; 2 days ago</p>
+                    </div>
+                    <button><img className='options' src={options} alt="" /></button>
+                </div>
+            </div>
+            <div className="card">
+                <div className="video">
+                    <img src={thumbnail3} alt="" />
+                </div>
+                <div className="video-info">
+                    <div className="video-info-left">
+                        <img src={channel} alt="" />
+                    </div>
+                    <div className="video-info-right">
+                        <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
+                        <h3>Greatstack</h3>
+                        <p>15k views &bull; 2 days ago</p>
+                    </div>
+                    <button><img className='options' src={options} alt="" /></button>
+                </div>
+            </div>
+            <div className="card">
+                <div className="video">
+                    <img src={thumbnail4} alt="" />
+                </div>
+                <div className="video-info">
+                    <div className="video-info-left">
+                        <img src={channel} alt="" />
+                    </div>
+                    <div className="video-info-right">
+                        <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
+                        <h3>Greatstack</h3>
+                        <p>15k views &bull; 2 days ago</p>
+                    </div>
+                    <button><img className='options' src={options} alt="" /></button>
+                </div>
+            </div>
+            <div className="card">
+                <div className="video">
+                    <img src={thumbnail5} alt="" />
+                </div>
+                <div className="video-info">
+                    <div className="video-info-left">
+                        <img src={channel} alt="" />
+                    </div>
+                    <div className="video-info-right">
+                        <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
+                        <h3>Greatstack</h3>
+                        <p>15k views &bull; 2 days ago</p>
+                    </div><button><img className='options' src={options} alt="" /></button>
+                </div>
+            </div>
+            <div className="card">
+                <div className="video">
+                    <img src={thumbnail6} alt="" />
+                </div>
+                <div className="video-info">
+                    <div className="video-info-left">
+                        <img src={channel} alt="" />
+                    </div>
+                    <div className="video-info-right">
+                        <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
+                        <h3>Greatstack</h3>
+                        <p>15k views &bull; 2 days ago</p>
+                    </div>
+                    <button><img className='options' src={options} alt="" /></button>
+                </div>
+            </div>
+            <div className="card">
+                <div className="video">
+                    <img src={thumbnail7} alt="" />
+                </div>
+                <div className="video-info">
+                    <div className="video-info-left">
+                        <img src={channel} alt="" />
+                    </div>
+                    <div className="video-info-right">
+                        <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
+                        <h3>Greatstack</h3>
+                        <p>15k views &bull; 2 days ago</p>
+                    </div>
+                    <button><img className='options' src={options} alt="" /></button>
+                </div>
+            </div>
+            <div className="card">
+                <div className="video">
+                    <img src={thumbnail8} alt="" />
+                </div>
+                <div className="video-info">
+                    <div className="video-info-left">
+                        <img src={channel} alt="" />
+                    </div>
+                    <div className="video-info-right">
+                        <h2>Best channel to learn coding that help you to be a webdeveloper</h2>
+                        <h3>Greatstack</h3>
+                        <p>15k views &bull; 2 days ago</p>
+                    </div>
+                    <button><img className='options' src={options} alt="" /></button>
+                </div>
+            </div>
+        </div >
+    )
 }
 
 export default Feed
