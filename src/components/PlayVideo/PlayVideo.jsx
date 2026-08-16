0a1adbded29863a9ea329d9613913d98e05152cb
Anmol Bhagat <anmolbhagat.dev@gmail.com>
2026-08-16 23:47:33 +0530

    style: add border radius to emoji icon

--- src/components/PlayVideo/PlayVideo.jsx
+++ src/components/PlayVideo/PlayVideo.jsx
@@ -16,6 +16,8 @@ import emoji from '../../assets/emoji.png'
 const PlayVideo = () => {
 
     const [cmt, setCmt] = useState(false)
+    const [bcng, setBcng] = useState(false)
+
     return (
         <div className='play-video'>
             <video src={video} controls autoPlay muted></video>
