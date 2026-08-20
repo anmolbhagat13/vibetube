7e93b60a88799db92df606e8efd2d16840bbee08
Anmol Bhagat <anmolbhagat.dev@gmail.com>
2026-08-20 20:17:17 +0530

    feat: show video options on card hover

--- src/components/Recommend/Recommend.jsx
+++ src/components/Recommend/Recommend.jsx
@@ -5,6 +5,64 @@ import tick from '../../assets/tick.svg'
 import play from '../../assets/play.svg'
 import options from '../../assets/options.svg'
 import { useSearchParams } from 'react-router-dom'
+
+const videos = [
+    {
+        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
+        channel: "Marvel HQ",
+        views: "16M",
+        time: "1y ago"
+    },
+    {
+        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
+        channel: "Marvel HQ",
+        views: "16M",
+        time: "1y ago"
+    },
+    {
+        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
+        channel: "Marvel HQ",
+        views: "16M",
+        time: "1y ago"
+    },
+    {
+        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
+        channel: "Marvel HQ",
+        views: "16M",
+        time: "1y ago"
+    },
+    {
+        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
+        channel: "Marvel HQ",
+        views: "16M",
+        time: "1y ago"
+    },
+    {
+        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
+        channel: "Marvel HQ",
+        views: "16M",
+        time: "1y ago"
+    },
+    {
+        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
+        channel: "Marvel HQ",
+        views: "16M",
+        time: "1y ago"
+    },
+    {
+        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
+        channel: "Marvel HQ",
+        views: "16M",
+        time: "1y ago"
+    },
+    {
+        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
+        channel: "Marvel HQ",
+        views: "16M",
+        time: "1y ago"
+    },
+];
+
 const Recommend = () => {
     return (
         <div className="vid-card-layout">
@@ -19,138 +77,33 @@ const Recommend = () => {
                     <button>Web Development</button>
                 </div>
             </div>
-            <div className="vid-card">
-                <div className="vc-left">
-                    <img src={thumbnail2} alt="" />
-                </div>
-                <div className="vc-right">
-                    <span className='vid-name'>All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode</span>
-                    <div className="vid-info">
-                        <div className="channel-name">
-                            <span>Marvel HQ</span>
-                            <img src={tick} alt="" />
+            {
+                videos.map((video) => (
+                    <div className="vid-card">
+                        <div className="vc-left">
+                            <img src={thumbnail2} alt="" />
                         </div>
-                        <div className="vid-details">
-                            <img src={play} alt="" />
-                            <span className='vid-views'>16M</span>
-                            <span className='vid-p-time'>1y ago</span>
-                        </div>
-                        <div className='options'>
-                            <img src={options} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="vid-card">
-                <div className="vc-left">
-                    <img src={thumbnail2} alt="" />
-                </div>
-                <div className="vc-right">
-                    <span className='vid-name'>All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode</span>
-                    <div className="vid-info">
-                        <div className="channel-name">
-                            <span>Marvel HQ</span>
-                            <img src={tick} alt="" />
-                        </div>
-                        <div className="vid-details">
-                            <img src={play} alt="" />
-                            <span className='vid-views'>16M</span>
-                            <span className='vid-p-time'>1y ago</span>
-                        </div>
-                        <div className='options'>
-                            <img src={options} alt="" />
+                        <div className="vc-right">
+                            <span className='vid-name'>{video.name}</span>
+                            <div className="vid-info">
+                                <div className="vc-channel-name">
+                                    <span>{video.channel}</span>
+                                    <img src={tick} alt="" />
+                                </div>
+                                <div className="vid-details">
+                                    <img src={play} alt="" />
+                                    <span className='vid-views'>{video.views}</span>
+                                    <span className='vid-p-time'>{video.time}</span>
+                                </div>
+                                <div className='options'>
+                                    <img src={options} alt="" />
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div className="vid-card">
-                <div className="vc-left">
-                    <img src={thumbnail2} alt="" />
-                </div>
-                <div className="vc-right">
-                    <span className='vid-name'>All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode</span>
-                    <div className="vid-info">
-                        <div className="channel-name">
-                            <span>Marvel HQ</span>
-                            <img src={tick} alt="" />
-                        </div>
-                        <div className="vid-details">
-                            <img src={play} alt="" />
-                            <span className='vid-views'>16M</span>
-                            <span className='vid-p-time'>1y ago</span>
-                        </div>
-                        <div className='options'>
-                            <img src={options} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="vid-card">
-                <div className="vc-left">
-                    <img src={thumbnail2} alt="" />
-                </div>
-                <div className="vc-right">
-                    <span className='vid-name'>All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode</span>
-                    <div className="vid-info">
-                        <div className="channel-name">
-                            <span>Marvel HQ</span>
-                            <img src={tick} alt="" />
-                        </div>
-                        <div className="vid-details">
-                            <img src={play} alt="" />
-                            <span className='vid-views'>16M</span>
-                            <span className='vid-p-time'>1y ago</span>
-                        </div>
-                        <div className='options'>
-                            <img src={options} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="vid-card">
-                <div className="vc-left">
-                    <img src={thumbnail2} alt="" />
-                </div>
-                <div className="vc-right">
-                    <span className='vid-name'>All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode</span>
-                    <div className="vid-info">
-                        <div className="channel-name">
-                            <span>Marvel HQ</span>
-                            <img src={tick} alt="" />
-                        </div>
-                        <div className="vid-details">
-                            <img src={play} alt="" />
-                            <span className='vid-views'>16M</span>
-                            <span className='vid-p-time'>1y ago</span>
-                        </div>
-                        <div className='options'>
-                            <img src={options} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="vid-card">
-                <div className="vc-left">
-                    <img src={thumbnail2} alt="" />
-                </div>
-                <div className="vc-right">
-                    <span className='vid-name'>All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode</span>
-                    <div className="vid-info">
-                        <div className="channel-name">
-                            <span>Marvel HQ</span>
-                            <img src={tick} alt="" />
-                        </div>
-                        <div className="vid-details">
-                            <img src={play} alt="" />
-                            <span className='vid-views'>16M</span>
-                            <span className='vid-p-time'>1y ago</span>
-                        </div>
-                        <div className='options'>
-                            <img src={options} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
+                ))
+            }
+
         </div>
     )
 }
