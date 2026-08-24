74d9ffcb12dd94fbe0c85df026029c78297902ed
Anmol Bhagat <anmolbhagat.dev@gmail.com>
2026-08-24 21:29:58 +0530

    feat: integrate API categories into Explore-section

--- src/components/Sidebar/ExploreSection/Explore.jsx
+++ src/components/Sidebar/ExploreSection/Explore.jsx
@@ -15,18 +15,22 @@ import podcasts from '../../../assets/vt-podcasts.png'
 import playables from '../../../assets/vt-playables.png'
 import memberships from '../../../assets/vt-memberships.png'
 
-const Explore = ({showMore,setShowMore, category,setCategory}) => {
+const Explore = ({ showMore, setShowMore, category, setCategory }) => {
     return (
         <div className="explore big">
             <span>Explore</span>
-            <div className={`side-link ${category===0?"active":""}`}onClick={()=>setCategory(0)}>
-                <img src={shopping} alt="" /> <p>Shopping</p>
-            </div>
-            <div className={`side-link ${category===10?"active":""}`} onClick={()=>setCategory(10)}>
+
+            <div className={`side-link ${category === 10 ? "active" : ""}`} onClick={() => setCategory(10)}>
                 <img src={music} alt="" /> <p>Music</p>
             </div>
-            <div className={`side-link ${category===30?"active":""}`} onClick={()=>setCategory(30)}>
-                <img src={moviestv} alt="" /> <p>Movies & TV</p>
+            <div className={`side-link ${category === 20 ? "active" : ""}`} onClick={() => setCategory(20)}>
+                <img src={gaming} alt="" /> <p>Gaming</p>
+            </div>
+            <div className={`side-link ${category === 17 ? "active" : ""}`} onClick={() => setCategory(17)}>
+                <img src={sports} alt="" /> <p>Sports</p>
+            </div>
+            <div className={`side-link ${category === 25 ? "active" : ""}`} onClick={() => setCategory(25)}>
+                <img src={news} alt="" /> <p>News</p>
             </div>
             {!showMore && (
                 <div onClick={() => { setShowMore(true) }} className="side-link">
@@ -35,32 +39,26 @@ const Explore = ({showMore,setShowMore, category,setCategory}) => {
             )}
             {showMore && (
                 <>
-                    <div className="side-link" onClick={()=>setCategory(0)}>
-                        <img src={live} alt="" /> <p>Live</p>
+                    <div className={`side-link ${category === 23 ? "active" : ""}`} onClick={() => setCategory(23)}>
+                        <img src={live} alt="" /> <p>Comedy</p>
                     </div>
-                    <div className={`side-link ${category===20?"active":""}`} onClick={()=>setCategory(20)}>
-                        <img src={gaming} alt="" /> <p>Gaming</p>
+                    <div className={`side-link ${category === 24 ? "active" : ""}`} onClick={() => setCategory(24)}>
+                        <img src={moviestv} alt="" /> <p>Entertainment</p>
                     </div>
-                    <div className={`side-link ${category===25?"active":""}`} onClick={()=>setCategory(25)}>
-                        <img src={news} alt="" /> <p>News</p>
+                    <div className={`side-link ${category === 28 ? "active" : ""}`} onClick={() => setCategory(28)}>
+                        <img src={shopping} alt="" /> <p>Science & Technology</p>
                     </div>
-                    <div className={`side-link ${category===17?"active":""}`} onClick={()=>setCategory(17)}>
-                        <img src={sports} alt="" /> <p>Sports</p>
+                    <div className={`side-link ${category === 2 ? "active" : ""}`} onClick={() => setCategory(2)}>
+                        <img src={playables} alt="" /> <p>Autos & Vehicles</p>
                     </div>
-                    <div className={`side-link ${category===0?"active":""}`} onClick={()=>setCategory(0)}>
-                        <img src={courses} alt="" /> <p>Courses</p>
+                    <div className={`side-link ${category === 26 ? "active" : ""}`} onClick={() => setCategory(26)}>
+                        <img src={fashionbeauty} alt="" /> <p>How-to & Style</p>
                     </div>
-                    <div className={`side-link ${category===0?"active":""}`} onClick={()=>setCategory(0)}>
-                        <img src={fashionbeauty} alt="" /> <p>Fashion & Beauty</p>
+                    <div className={`side-link ${category === 1 ? "active" : ""}`} onClick={() => setCategory(1)}>
+                        <img src={podcasts} alt="" /> <p>Film & Animation</p>
                     </div>
-                    <div className={`side-link ${category===0?"active":""}`} onClick={()=>setCategory(0)}>
-                        <img src={podcasts} alt="" /> <p>Podcasts</p>
-                    </div>
-                    <div className={`side-link ${category===0?"active":""}`} onClick={()=>setCategory(0)}>
-                        <img src={playables} alt="" /> <p>Playables</p>
-                    </div>
-                    <div className={`side-link ${category===0?"active":""}`} onClick={()=>setCategory(0)}>
-                        <img src={memberships} alt="" /> <p>Memberships</p>
+                    <div className={`side-link ${category === 22 ? "active" : ""}`} onClick={() => setCategory(22)}>
+                        <img src={courses} alt="" /> <p>People & Blogs</p>
                     </div>
                     <div onClick={() => { setShowMore(false) }} className="side-link">
                         <img src={showless} alt="" /> <p>Show less</p>
--- src/components/Sidebar/HomeSection/HomeSection.jsx
+++ src/components/Sidebar/HomeSection/HomeSection.jsx
@@ -5,19 +5,19 @@ import shorts from '../../../assets/vt-shorts.png'
 import user from '../../../assets/vt-user.svg'
 import subs from '../../../assets/vt-subs.png'
 
-const HomeSection = (category,setCategory) => {
+const HomeSection = ({category,setCategory}) => {
     return (
         <div className="shortcut-links">
             <div className={`side-link ${category===0?"active":""}`} onClick={()=>setCategory(0)}>
                 <img src={house} alt="" /><p>Home</p>
             </div>
-            <div className="side-link" onClick={()=>setCategory(42)}>
+            <div className="side-link">
                 <img src={shorts} alt="" /><p>Shorts</p>
             </div>
-            <div className="side-link small-side-link" onClick={()=>setCategory(0)}>
+            <div className="side-link small-side-link">
                 <img src={subs} alt="" /> <p>Subscriptions</p>
             </div>
-            <div className="side-link small-side-link" onClick={()=>setCategory(0)}>
+            <div className="side-link small-side-link">
                 <img src={user} alt="" /> <p>You</p>
             </div>
         </div>
