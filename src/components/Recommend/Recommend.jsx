d4e43945b30d5b23690b55b58835456b257b1d27
Anmol Bhagat <anmolbhagat.dev@gmail.com>
2026-08-27 00:12:47 +0530

    add dynamic video data to recommendations

--- src/components/Recommend/Recommend.jsx
+++ src/components/Recommend/Recommend.jsx
@@ -1,71 +1,64 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Recommend.css'
-import thumbnail2 from '../../assets/thumbnail2.png'
 import tick from '../../assets/tick.svg'
 import play from '../../assets/play.svg'
 import options from '../../assets/options.svg'
-import { useSearchParams } from 'react-router-dom'
-
-const videos = [
-    {
-        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
-        channel: "Marvel HQ",
-        views: "16M",
-        time: "1y ago"
-    },
-    {
-        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
-        channel: "Marvel HQ",
-        views: "16M",
-        time: "1y ago"
-    },
-    {
-        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
-        channel: "Marvel HQ",
-        views: "16M",
-        time: "1y ago"
-    },
-    {
-        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
-        channel: "Marvel HQ",
-        views: "16M",
-        time: "1y ago"
-    },
-    {
-        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
-        channel: "Marvel HQ",
-        views: "16M",
-        time: "1y ago"
-    },
-    {
-        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
-        channel: "Marvel HQ",
-        views: "16M",
-        time: "1y ago"
-    },
-    {
-        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
-        channel: "Marvel HQ",
-        views: "16M",
-        time: "1y ago"
-    },
-    {
-        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
-        channel: "Marvel HQ",
-        views: "16M",
-        time: "1y ago"
-    },
-    {
-        name: "All Father's Day | Marvel's Avengers Assemble S1 E20 | Full Episode",
-        channel: "Marvel HQ",
-        views: "16M",
-        time: "1y ago"
-    },
-];
+import { Link, useParams } from 'react-router-dom'
+import { API_KEY } from '../../data'
+import { formatViews, timeAgo } from '../../utils/format'
 
 const Recommend = () => {
+
+    const [data, setData] = useState([])
+    const [channelData, setChannelData] = useState({})
+
+    const { categoryId, videoId } = useParams()
+
+    const fetchData = async () => {
+
+        const videoList_url =
+            `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=25&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
+
+        const response = await fetch(videoList_url)
+        const result = await response.json()
+
+        const filteredVideos = result.items.filter(
+            item => item.id !== videoId
+        )
+
+        setData(filteredVideos)
+
+        const channelIds = [
+            ...new Set(
+                filteredVideos.map(item => item.snippet.channelId)
+            )
+        ].join(',')
+
+        if (!channelIds) return
+
+        const channelResponse = await fetch(
+            `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelIds}&key=${API_KEY}`
+        )
+
+        const channelResult = await channelResponse.json()
+
+        const channels = {}
+
+        channelResult.items.forEach(channel => {
+            channels[channel.id] =
+                channel.snippet.thumbnails.default.url
+        })
+
+        setChannelData(channels)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [categoryId, videoId])
+
     return (
         <div className="vid-card-layout">
+
             <div className="vid-recommend">
                 <div className="all-btn btn">
                     <button>All</button>
@@ -78,29 +71,59 @@ const Recommend = () => {
                 </div>
             </div>
             {
-                videos.map((video, index) => (
-                    <div key={index} className="vid-card">
+                data.map((item) => (
+                    <Link
+                        key={item.id}
+                        to={`/video/${item.snippet.categoryId}/${item.id}`}
+                        className="vid-card"
+                    >
+
                         <div className="vc-left">
-                            <img src={thumbnail2} alt="" />
+                            <img
+                                src={item.snippet.thumbnails.medium.url}
+                                alt=""
+                            />
                         </div>
+
                         <div className="vc-right">
-                            <span className='vid-name'>{video.name}</span>
+
+                            <span className="vid-name">
+                                {item.snippet.title}
+                            </span>
+
                             <div className="vid-info">
+
                                 <div className="vc-channel-name">
-                                    <span>{video.channel}</span>
+                                    <span>
+                                        {item.snippet.channelTitle}
+                                    </span>
+
                                     <img src={tick} alt="" />
                                 </div>
+
                                 <div className="vid-details">
+
                                     <img src={play} alt="" />
-                                    <span className='vid-views'>{video.views}</span>
-                                    <span className='vid-p-time'>{video.time}</span>
+
+                                    <span className="vid-views">
+                                        {formatViews(item.statistics.viewCount)}
+                                    </span>
+
+                                    <span className="vid-p-time">
+                                        {timeAgo(item.snippet.publishedAt)}
+                                    </span>
+
                                 </div>
-                                <div className='options'>
+
+                                <div className="options">
                                     <img src={options} alt="" />
                                 </div>
+
                             </div>
+
                         </div>
-                    </div>
+
+                    </Link>
                 ))
             }
 
