bd5d48d074557da575d1440d9faf27bb591db594
Anmol Bhagat <anmolbhagat.dev@gmail.com>
2026-08-28 23:57:23 +0530

    refactor:  update formatting utility

--- src/utils/format.js
+++ src/utils/format.js
@@ -18,40 +18,40 @@ export const timeAgo = (date) => {
     )
 
     if (seconds < 60) {
-        return `${seconds} seconds ago`
+        return `${seconds}s ago`
     }
 
     const minutes = Math.floor(seconds / 60)
 
     if (minutes < 60) {
-        return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`
+        return `${minutes}m ago`
     }
 
     const hours = Math.floor(minutes / 60)
 
     if (hours < 24) {
-        return `${hours} hour${hours !== 1 ? 's' : ''} ago`
+        return `${hours}h ago`
     }
 
     const days = Math.floor(hours / 24)
 
     if (days < 7) {
-        return `${days} day${days !== 1 ? 's' : ''} ago`
+        return `${days}d ago`
     }
 
     const weeks = Math.floor(days / 7)
 
     if (weeks < 5) {
-        return `${weeks} week${weeks !== 1 ? 's' : ''} ago`
+        return `${weeks}w ago`
     }
 
     const months = Math.floor(days / 30)
 
     if (months < 12) {
-        return `${months} month${months !== 1 ? 's' : ''} ago`
+        return `${months}mo ago`
     }
 
     const years = Math.floor(days / 365)
 
-    return `${years} year${years !== 1 ? 's' : ''} ago`
+    return `${years}y ago`
 }
