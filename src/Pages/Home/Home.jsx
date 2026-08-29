bee54ee675b99ad9ea5250b050421fba4c3572cf
Anmol Bhagat <anmolbhagat.dev@gmail.com>
2026-08-29 23:41:14 +0530

    feat: make home page responsive

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react'
 import './Home.css'
-import Sidebar  from '../../components/Sidebar/Sidebar'
+import Sidebar from '../../components/Sidebar/Sidebar'
 import Feed from '../../components/Feed/Feed'
 
-const Home = ({sidebar}) => {
+const Home = ({ sidebar }) => {
 
   const [category, setCategory] = useState(0)
 
   return (
-    <>
-    <Sidebar sidebar={sidebar} category={category} setCategory={setCategory} />
-    <div className={`container content ${sidebar?"":'large-container content'}`}>
-      <Feed category={category} />
+    <div className='home'>
+      {/* <div className='sidebar'> */}
+        <Sidebar sidebar={sidebar} category={category} setCategory={setCategory} />
+      {/* </div> */}
+      <div className={`container content ${sidebar ? "" : 'large-container content'}`}>
+        <Feed category={category} />
+      </div>
     </div>
-  </>
   )
 }
 
